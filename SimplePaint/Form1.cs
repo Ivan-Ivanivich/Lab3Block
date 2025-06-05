a63fed3aa58dd160153bf5757af64e2d847752d7
ip <ip969789@gmail.com>
2025-06-06 00:55:41 +0300

    Edit form1

--- SimplePaint/Form1.cs
+++ SimplePaint/Form1.cs
@@ -16,17 +16,17 @@ namespace SimplePaint
         public Form1()
         {
             InitializeComponent();
-            CreateBlank( pictureBox1.Width, pictureBox1.Height );
+            CreateBlank(pictureBox1.Width, pictureBox1.Height);
         }
         //цвет заливки по умолчанию 
         Color DefaultColor
         {
-            get { return Color.Aqua; }
+            get { return Color.White; } //Aqua
         }
 
         //создаем пустую облость рисования 
         void CreateBlank(int width, int height)
-        { 
+        {
             //сохраняем старую картинку 
             var oldImage = pictureBox1.Image;
             //созжаем новый битмап
@@ -43,16 +43,57 @@ namespace SimplePaint
 
             pictureBox1.Image = bmp;
             if (oldImage != null)
-            { 
+            {
                 oldImage.Dispose();// освобождаем ресурсы занятые старой картинкой
             }
-
-        
         }
 
+        //найти ошибки не рисует 
+        int _x;
+        int _y;
+        bool _mouseClicked = false;
+        Color SelectedColor
+        { 
+            get { return Color.Red; }
+        }
+        int SelectedSize
+        {
+            get { return sizeTrackBar.Value; } //изменено trackBar1
+        }
+        Brush _selectedBrush;
 
+        //_selectedBrush = new QuadBrush(SelectedColor, SelectedSize);
+        private void button1_Click(object sender, EventArgs e) //Button1
+        {
+            _selectedBrush = new QuadBrush(SelectedColor, SelectedSize);
+        }
 
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_selectedBrush == null) { return; }
 
-     
+            _selectedBrush.Draw(pictureBox1.Image as Bitmap, _x, _y);
+            pictureBox1.Refresh();
+
+            _mouseClicked = true;   
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        { 
+            _mouseClicked = false ;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        { 
+            _x = e.X > 0 ? e.X : 0;
+            _y = e.Y > 0 ? e.Y : 0;
+            if (_mouseClicked)
+            {
+                _selectedBrush.Draw(pictureBox1.Image as Bitmap, _x, _y);
+                pictureBox1.Refresh();
+            }
+        }
     }
 }
+
+
