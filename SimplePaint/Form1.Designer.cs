4c02192734255f1bd0e2313b495ba7db63abc0a9
kalos <ip969789@gmail.com>
2025-06-07 12:32:44 +0300

    edit with coach

--- SimplePaint/Form1.Designer.cs
+++ SimplePaint/Form1.Designer.cs
@@ -49,8 +49,9 @@
             this.toolsPanel.Controls.Add(this.brushGroupBox);
             this.toolsPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.toolsPanel.Location = new System.Drawing.Point(0, 0);
+            this.toolsPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.toolsPanel.Name = "toolsPanel";
-            this.toolsPanel.Size = new System.Drawing.Size(200, 451);
+            this.toolsPanel.Size = new System.Drawing.Size(267, 555);
             this.toolsPanel.TabIndex = 1;
             // 
             // brushGroupBox
@@ -62,57 +63,65 @@
             this.brushGroupBox.Controls.Add(this.sizeTrackBar);
             this.brushGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.brushGroupBox.Location = new System.Drawing.Point(0, 0);
+            this.brushGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.brushGroupBox.Name = "brushGroupBox";
-            this.brushGroupBox.Size = new System.Drawing.Size(200, 129);
+            this.brushGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.brushGroupBox.Size = new System.Drawing.Size(267, 159);
             this.brushGroupBox.TabIndex = 0;
             this.brushGroupBox.TabStop = false;
             this.brushGroupBox.Text = "Выбор кисти";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(105, 100);
+            this.button4.Location = new System.Drawing.Point(140, 123);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 4;
             this.button4.Text = "Кнопка 4 lol";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 100);
+            this.button3.Location = new System.Drawing.Point(16, 123);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 3;
             this.button3.Text = "Круг";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(105, 62);
+            this.button2.Location = new System.Drawing.Point(140, 76);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Прямоугольник";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 62);
+            this.button1.Location = new System.Drawing.Point(16, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Квадрат";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // sizeTrackBar
             // 
             this.sizeTrackBar.AutoSize = false;
             this.sizeTrackBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.sizeTrackBar.Location = new System.Drawing.Point(3, 16);
+            this.sizeTrackBar.Location = new System.Drawing.Point(4, 19);
+            this.sizeTrackBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sizeTrackBar.Maximum = 50;
             this.sizeTrackBar.Minimum = 1;
             this.sizeTrackBar.Name = "sizeTrackBar";
-            this.sizeTrackBar.Size = new System.Drawing.Size(194, 40);
+            this.sizeTrackBar.Size = new System.Drawing.Size(259, 49);
             this.sizeTrackBar.TabIndex = 0;
             this.sizeTrackBar.Value = 1;
             // 
@@ -120,27 +129,33 @@
             // 
             this.drawPanel.Controls.Add(this.pictureBox1);
             this.drawPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.drawPanel.Location = new System.Drawing.Point(200, 0);
+            this.drawPanel.Location = new System.Drawing.Point(267, 0);
+            this.drawPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.drawPanel.Name = "drawPanel";
-            this.drawPanel.Size = new System.Drawing.Size(557, 451);
+            this.drawPanel.Size = new System.Drawing.Size(742, 555);
             this.drawPanel.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(557, 451);
+            this.pictureBox1.Size = new System.Drawing.Size(742, 555);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(757, 451);
+            this.ClientSize = new System.Drawing.Size(1009, 555);
             this.Controls.Add(this.drawPanel);
             this.Controls.Add(this.toolsPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.toolsPanel.ResumeLayout(false);
